287b48e3c67e05db19677306a5ffbd719c93b4cb
kumar889 <kumar.bajura@gmail.com>
2021-06-15 18:47:57 +0545

    Question9,commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let myObj = {};
+
+let a = "apple";
+let b = "red";
+let c = "ball";
+let d = "round";
+let e = "cat";
+let f = "meow";
+let g = "dog";
+let h = "bone";
+
+myObj['a'] = a;
+
+console.log(myObj);
